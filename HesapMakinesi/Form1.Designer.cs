8f5f17086170d63fa26a1928678346a6ea5ca82e
yildizemre2021 <yildiz.emre.2021@gmail.com>
2022-06-12 21:20:53 +0300

    Dizayn yapilir

--- HesapMakinesi/Form1.Designer.cs
+++ HesapMakinesi/Form1.Designer.cs
@@ -28,12 +28,254 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btnArti = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnEksi = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btnCarpi = new System.Windows.Forms.Button();
+            this.btnEsit = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnSilme = new System.Windows.Forms.Button();
+            this.btnBolu = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.hesapAlani = new System.Windows.Forms.TextBox();
+            this.islem1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(12, 168);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 50);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn2.Location = new System.Drawing.Point(93, 168);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 50);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btnArti
+            // 
+            this.btnArti.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnArti.Location = new System.Drawing.Point(255, 168);
+            this.btnArti.Name = "btnArti";
+            this.btnArti.Size = new System.Drawing.Size(75, 50);
+            this.btnArti.TabIndex = 3;
+            this.btnArti.Text = "+";
+            this.btnArti.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn3.Location = new System.Drawing.Point(174, 168);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 50);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btnEksi
+            // 
+            this.btnEksi.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEksi.Location = new System.Drawing.Point(255, 224);
+            this.btnEksi.Name = "btnEksi";
+            this.btnEksi.Size = new System.Drawing.Size(75, 50);
+            this.btnEksi.TabIndex = 7;
+            this.btnEksi.Text = "-";
+            this.btnEksi.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn6.Location = new System.Drawing.Point(174, 224);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 50);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn5.Location = new System.Drawing.Point(93, 224);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 50);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn4.Location = new System.Drawing.Point(12, 224);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 50);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btnCarpi
+            // 
+            this.btnCarpi.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCarpi.Location = new System.Drawing.Point(255, 280);
+            this.btnCarpi.Name = "btnCarpi";
+            this.btnCarpi.Size = new System.Drawing.Size(75, 50);
+            this.btnCarpi.TabIndex = 15;
+            this.btnCarpi.Text = "*";
+            this.btnCarpi.UseVisualStyleBackColor = true;
+            // 
+            // btnEsit
+            // 
+            this.btnEsit.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEsit.Location = new System.Drawing.Point(174, 336);
+            this.btnEsit.Name = "btnEsit";
+            this.btnEsit.Size = new System.Drawing.Size(75, 50);
+            this.btnEsit.TabIndex = 14;
+            this.btnEsit.Text = "=";
+            this.btnEsit.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn0.Location = new System.Drawing.Point(93, 336);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(75, 50);
+            this.btn0.TabIndex = 13;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            // 
+            // btnSilme
+            // 
+            this.btnSilme.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSilme.Location = new System.Drawing.Point(12, 336);
+            this.btnSilme.Name = "btnSilme";
+            this.btnSilme.Size = new System.Drawing.Size(75, 50);
+            this.btnSilme.TabIndex = 12;
+            this.btnSilme.Text = "C";
+            this.btnSilme.UseVisualStyleBackColor = true;
+            // 
+            // btnBolu
+            // 
+            this.btnBolu.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnBolu.Location = new System.Drawing.Point(258, 336);
+            this.btnBolu.Name = "btnBolu";
+            this.btnBolu.Size = new System.Drawing.Size(75, 50);
+            this.btnBolu.TabIndex = 11;
+            this.btnBolu.Text = "/";
+            this.btnBolu.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn9.Location = new System.Drawing.Point(174, 280);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 50);
+            this.btn9.TabIndex = 10;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn8.Location = new System.Drawing.Point(93, 280);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 50);
+            this.btn8.TabIndex = 9;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn7.Location = new System.Drawing.Point(12, 280);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 50);
+            this.btn7.TabIndex = 8;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // hesapAlani
+            // 
+            this.hesapAlani.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.hesapAlani.Location = new System.Drawing.Point(12, 108);
+            this.hesapAlani.Name = "hesapAlani";
+            this.hesapAlani.Size = new System.Drawing.Size(318, 35);
+            this.hesapAlani.TabIndex = 16;
+            this.hesapAlani.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // islem1
+            // 
+            this.islem1.AutoSize = true;
+            this.islem1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.islem1.Location = new System.Drawing.Point(276, 75);
+            this.islem1.Name = "islem1";
+            this.islem1.Size = new System.Drawing.Size(68, 30);
+            this.islem1.TabIndex = 17;
+            this.islem1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(345, 421);
+            this.Controls.Add(this.islem1);
+            this.Controls.Add(this.hesapAlani);
+            this.Controls.Add(this.btnCarpi);
+            this.Controls.Add(this.btnEsit);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnSilme);
+            this.Controls.Add(this.btnBolu);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btnEksi);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btnArti);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btn1;
+        private Button btn2;
+        private Button btnArti;
+        private Button btn3;
+        private Button btnEksi;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btnCarpi;
+        private Button btnEsit;
+        private Button btn0;
+        private Button btnSilme;
+        private Button btnBolu;
+        private Button btn9;
+        private Button btn8;
+        private Button btn7;
+        private TextBox hesapAlani;
+        private Label islem1;
     }
 }
